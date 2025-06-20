aa1a454429470e888a372202c900a194be059611
lucasmagalhaes33 <lucas.magalhaes@castanhal.ufpa.br>
2025-06-20 15:08:17 -0300

    feat: iniciando o projeto com Spring Boot e adição do gitignore.

---
+++ src/main/java/com/example/bancong/BancoNgApplication.java
@@ -0,0 +1,13 @@
+package com.example.bancong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoNgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancoNgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bancong/BancoNgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bancong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoNgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
